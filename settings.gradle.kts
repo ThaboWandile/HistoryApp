5e45aa7b0094713d410185c7cbc4a914da3f1aae
ThaboWandile <163864914+ThaboWandile@users.noreply.github.com>
2024-04-05 20:51:47 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "historyapp2"
+include(":app")
+ 
